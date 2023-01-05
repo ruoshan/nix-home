df37d295f92c4fb1e93e2ddf91d1f7f29cb7643a
Ruoshan Huang <ruoshan.huang@gmail.com>
2023-01-05 19:12:02 +0800

    Fix zsh

--- home.nix
+++ home.nix
@@ -13,6 +13,8 @@
   ];
 
   home.packages = with pkgs; [
+    exa
+    bat
     bashInteractive
     fzf
     ripgrep
--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -6,7 +6,17 @@
       enable = true;
       prompt.theme = "powerlevel10k";
     };
+    envExtra = ''
+      export PATH="$PATH:$HOME/.local/bin"
+    '';
     initExtra = builtins.readFile ./zshrc;
+    profileExtra = ''
+      # Nix
+      if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+        . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+        export MANPATH="$HOME/.nix-profile/share/man:/nix/var/nix/profiles/default/share/man:$MANPATH"
+      fi
+    '';
   };
 
   home.file.".p10k.zsh".source = ./p10k.zsh;
